b6cd01e5f6da33f2882894a18c98061d618d78b3
Roger Marvin <20122077@kaishi-pu.ac.jp>
2024-06-05 14:52:51 +0900

    update security

--- Hooks/useResasAPI.js
+++ Hooks/useResasAPI.js
@@ -6,31 +6,30 @@ function useResasApi() {
     const [cityCode, setCityCode] = useState('');
     const [prefCode, setPrefCode] = useState('');
     const [category, setCategory] = useState('');
+
     useEffect(() => {
         if (cityCode && prefCode && category) {
-            const apiKey = "SYCXoS2nj8kRZ9uFWFz39pWUsUcQOIvGWCCkuuYK";
+            const apiKey = import.meta.env.VITE_API_KEY;
             const headers = { "X-API-KEY": apiKey };
             const url = `https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?cityCode=${cityCode}&prefCode=${prefCode}`;
 
-            axios
-                .get(url, { headers })
-                .then((response) => {
-                    const allData = response.data.result.data.flatMap((category) =>
-                        category.data.map((item) => ({
+            axios.get(url, { headers })
+                .then(response => {
+                    const allData = response.data.result.data.flatMap(cat =>
+                        cat.data.map(item => ({
                             year: item.year,
-                            label: category.label,
+                            label: cat.label,
                             value: item.value,
                             rate: item.rate ? `${item.rate}%` : "N/A",
                         }))
                     );
-                    const filteredData = category ? allData.filter(item => item.label === category) : allData;
+                    const filteredData = allData.filter(item => item.label === category);
                     setData(filteredData);
                 })
-                .catch((error) => {
+                .catch(error => {
                     console.error("Error fetching data:", error);
                 });
         }
-
     }, [cityCode, prefCode, category]);
 
     return { data, setCityCode, setPrefCode, setCategory };
